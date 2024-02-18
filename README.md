d133fa8903ffa9a22c80ba06e651880cc3e77394
ralphmb <ralphbraithwaite1@gmail.com>
2024-02-18 23:01:03 +0000

    Tidied up code, removed mouse move checking, added small helper script for easier testing/installation

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 This extension for Spicetify contains a Javascript list in the source code where you can enter the names of any options you want to block from appearing in the carousel. Edit this list to your preference. By default I have removed *Podcasts*, *Artists*, *By you*, and *By Spotify*.
 It's slightly janky - I'm no web developer and had to relearn a lot of very basic JS to make this. Any suggestions please let me know.
 This extension is working on MacOS 14.2.1 and was made for Spotify v2.31.3, Spicetify v1.2.17.
+`move_script` is a small zsh script that copies the .js file to the Spicetify extensions folder, then calls `spicetify apply && spicetify enable-devtools`.
 
 ## Installation
-Assuming you have Spotify/Spicetify installed already - copy the file `pill_remover.js` into the Spicetify extensions directory. On MacOS this is located at `/Users/firstnamelastname/.config/spicetify/Extensions/pill_remover.js` by default.
+Assuming you have Spotify/Spicetify installed already - copy the file `pill_remover.js` into the Spicetify extensions directory. On MacOS this is located at `~/.config/spicetify/Extensions/pill_remover.js` by default.
 Then add it to your Spicetify extensions using
 ```shell
 spicetify config extensions pill_remover.js
--- pill_remover.js
+++ pill_remover.js
@@ -1,7 +1,8 @@
-// <reference path="../globals.d.ts">
-
+// TODO
+// Find a better way of locating the pill buttons
+// Figure out a better event to listen to - DOMContentLoaded?
+//
 (async function remove_unused_pills() {
-  console.log("remove_unused_pills loaded");
   function remover() {
     // Add the exact name of the pills you want to hide here
     var pills_to_remove = [
@@ -11,9 +12,11 @@
       "By Spotify",
     ];
     // Gets a list of all pill buttons in the carousel
-    var visible_pill_buttons = document.getElementsByClassName(
-      "ChipInner__ChipInnerComponent-sc-1ly6j4j-0 ChipInnerComponent main-useDropTarget-base main-useDropTarget-track main-useDropTarget-local main-useDropTarget-album main-useDropTarget-artist main-useDropTarget-playlist main-useDropTarget-folder main-useDropTarget-episode OxrHhBsQAggwnv6RmqRy main-useDropTarget-show main-useDropTarget-audiobook main-useDropTarget-pseudoPlaylist",
-    );
+
+    var target_classname =
+      "ChipInner__ChipInnerComponent-sc-1ly6j4j-0 ChipInnerComponent main-useDropTarget-base main-useDropTarget-track main-useDropTarget-local main-useDropTarget-album main-useDropTarget-artist main-useDropTarget-playlist main-useDropTarget-folder main-useDropTarget-episode OxrHhBsQAggwnv6RmqRy main-useDropTarget-show main-useDropTarget-audiobook main-useDropTarget-pseudoPlaylist";
+    var visible_pill_buttons =
+      document.getElementsByClassName(target_classname);
     // loops through the list
     for (btn in visible_pill_buttons) {
       // gets the text contained in each pill
@@ -26,33 +29,30 @@
       }
     }
   }
-  // Currently it listens for clicks and mouse moves before pruning.
+
+  console.log("Pill button remover loaded");
+
+  // Currently it listens for clicks before pruning.
   // The reason for the async await 10/20/30ms listeners is to re-remove any bad options
   // after the user clicks on one. It feels like a janky way to do this and you can see
   // that visually it doesn't look great but I am no web dev.
   // If the timer is too short it won't remove them all - some won't have rendered yet
   // If too long it looks ugly - so the compromise is 3 different callbacks.
+  //
   document.addEventListener("click", () => {
     console.log("Remover called - click");
     remover();
   });
-  document.addEventListener("mousemove", () => {
-    console.log("Remover called - mouse move");
-    remover();
-  });
   document.addEventListener("click", async () => {
     await new Promise((r) => setTimeout(r, 10));
-    console.log("Remover called - 10ms after click");
     remover();
   });
   document.addEventListener("click", async () => {
     await new Promise((r) => setTimeout(r, 20));
-    console.log("Remover called - 20ms after click");
     remover();
   });
   document.addEventListener("click", async () => {
     await new Promise((r) => setTimeout(r, 30));
-    console.log("Remover called - 30ms after click");
     remover();
   });
 })();
